638ceb5685b3a05c78049f03eaceb6d51fd520a3
bhaskar <b.bhaskar.nair@gmail.com>
2021-10-21 11:23:25 +0530

    added scroll function

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,30 @@ import './App.css'
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { counter: 0, offsets: [] };
-    this.calculateSectionOffsets()
+
+    this.state = {
+      inMove: false,
+      activeSection: 0,
+      offsets: [],
+      touchStartY: 0
+    };
+
+
+    // window.addEventListener("touchstart", this.touchStart, { passive: false }); // mobile devices
+    // window.addEventListener("touchmove", this.touchMove, { passive: false }); // mobile devices
   }
 
-  calculateSectionOffsets = async function () {
+  componentDidMount() {
+    // Full-page scroll code
+    this.calculateSectionOffsets()
+
+    window.addEventListener("DOMMouseScroll", this.handleMouseWheel); // Mozilla Firefox
+    window.addEventListener("mousewheel", this.handleMouseWheel, {
+      passive: false
+    }); // Other browsers
+  }
+
+  calculateSectionOffsets = async () => {
     let sections = await document.getElementsByTagName("section");
     let length = sections.length;
 
@@ -18,11 +37,52 @@ class App extends React.Component {
       let sectionOffset = sections[i].offsetTop;
       this.state.offsets.push(sectionOffset);
     }
-    console.log(this.state.offsets);
+  }
+
+  handleMouseWheel = (e) => {
+    const wheelLoc = e.wheelDelta || e.detail
+    if (wheelLoc < 30 && !this.state.inMove) {
+      this.moveUp();
+    } else if (wheelLoc > 30 && !this.state.inMove) {
+      this.moveDown();
+    }
+    e.preventDefault();
+    return false;
+  }
+
+  moveDown = () => {
+    this.setState({ inMove: true })
+    this.state.activeSection--;
+    if (this.state.activeSection < 0)
+      this.setState((state, props) => ({
+        activeSection: state.offsets.length - 1
+      }));
+    this.scrollToSection(this.state.activeSection, true);
+  }
+
+  moveUp = () => {
+    const section = this.state.activeSection
+    this.setState({ inMove: true, activeSection: section + 1 })
+
+    if (this.state.activeSection > this.state.offsets.length - 1)
+      this.setState({ activeSection: 0 });
+
+    this.scrollToSection(this.state.activeSection, true);
+  }
+
+  scrollToSection = (id, force = false) => {
+    if (this.state.inMove && !force) return false;
+    this.setState({ activeSection: id, inMove: true })
+
+    const currSection = document.getElementsByTagName("section")[id]
+    currSection.scrollIntoView({ behavior: "smooth" });
+
+    setTimeout(() => {
+      this.setState({ inMove: false })
+    }, 400);
   }
 
   render() {
-
     return (
       <div className="App" >
         <Navigation></Navigation>

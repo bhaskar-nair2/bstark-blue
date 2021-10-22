80d31157e1c7019d48eb01e5de7754136479d8ca
bhaskar <b.bhaskar.nair@gmail.com>
2021-10-22 11:09:21 +0530

    updated favicon & meta

--- src/main.jsx
+++ src/main.jsx
@@ -1,26 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import DocumentMeta from 'react-document-meta';
-
-import './index.css'
-import App from './App'
 import GA4React from "ga-4-react";
 
-const meta = {
-  title: 'Bstark Blue | Bhaskar Nair',
-  description: 'Bhaskar Nair, Frontend Architect, Full Stack Developer, Hyderabad, India',
-  canonical: 'https://www.bstark.tech',
-  meta: {
-    charset: 'utf-8',
-    name: {
-      keywords: 'bhaskar nair, developer, india, hyderabad, front end, full stack'
-    }
-  }
-};
+import App from './App'
+
+import './index.css'
 
 ReactDOM.render(
   <React.StrictMode>
-    <DocumentMeta {...meta} />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
